85b28aed31a6991122c6f0f840509929a80555e9
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-10 19:00:49 +0530

    updated

---
+++ application/logs/log-2024-10-10.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-10-10 13:25:55 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'mediamanager' C:\wamp64\www\ci-gallery-manager\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2024-10-10 13:25:56 --> Severity: error --> Exception: Unable to connect to the database. C:\wamp64\www\ci-gallery-manager\system\database\DB_driver.php 433
